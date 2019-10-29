b4dc65cb6d7e7427d2402c3efe5497d84bdbb9fc
Christian Kuri <christian.kuri.martinez@gmail.com>
2019-10-28 20:11:16 -0600

    Initial Minesweeper structure

---
+++ database/factories/MinesweeperFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Minesweeper;
+use Faker\Generator as Faker;
+
+$factory->define(Minesweeper::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
