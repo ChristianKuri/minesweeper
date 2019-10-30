061a909583d83a83a04272730c8cadb5b4df2188
Christian Kuri <christian.kuri.martinez@gmail.com>
2019-10-29 21:20:26 -0600

    Update to Smaller grid

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-7">
             <div class="card">
                 <div class="card-header">Minesweeper</div>
 
@@ -20,7 +20,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-5">
             <div class="card">
                 <div class="card-header">Instructions</div>
                 <div class="card-body">
